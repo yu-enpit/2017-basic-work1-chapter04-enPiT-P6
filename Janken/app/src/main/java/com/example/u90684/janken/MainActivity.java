421e70a3b0aafbf2439f10b0db5de0ef8b34a38e
enPiT-P6 <v013ff@yamaguchi-u.ac.jp>
2017-07-12 00:16:34 +0900

    教科書通りに行いました。 なかなか実行できず初めの設定(レイアウトの部分)の部分で足をすくわれました。

--- Janken/app/src/main/java/com/example/u90684/janken/MainActivity.java
+++ Janken/app/src/main/java/com/example/u90684/janken/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.u90684.janken;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//起動時にデータをクリアする。
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+    }
+    public void onJanKenButtonTapped(View view){
+        Intent intent = new Intent(this,ResultActivity.class);
+        int id = intent.getIntExtra("MY_HAND",view.getId());
+        intent.putExtra("MY_HAND",view.getId());
+        startActivity(intent);
     }
 }
